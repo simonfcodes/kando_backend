478000b5f40f9b0134f86ef58df89d0f88a4f8a2
Simon F <simon@simoncodes.dev>
2025-09-09 02:47:48 +0100

    Initial maven project creation

---
+++ src/main/java/dev/simoncodes/kando_backend/KandoBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.simoncodes.kando_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KandoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KandoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/simoncodes/kando_backend/KandoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simoncodes.kando_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KandoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
